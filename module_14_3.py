4ed554b5e25f7c4b54e0a7cb9dd380b5527d54c3
EniaLui <eniana@mail.ru>
2024-12-28 12:16:41 +0300

    Add files via upload

---
+++ module_14_3.py
@@ -0,0 +1,96 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton, InputFile
+
+api = "7501195936:AAFX38VsGr1Birsl-ieYcuT6wv3TJCnKoTE"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(KeyboardButton("Рассчитать"), KeyboardButton("Информация"))
+keyboard.add(KeyboardButton("Купить"))
+inline_kb = InlineKeyboardMarkup(row_width=1)
+inline_kb.add(InlineKeyboardButton("Рассчитать норму калорий", callback_data='calories'),
+                      InlineKeyboardButton("Формулы расчёта", callback_data='formulas'))
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+
+    await message.answer(
+        "Привет! Я бот, помогающий твоему здоровью.\nНажмите 'Рассчитать', чтобы начать расчет нормы калорий.",
+        reply_markup=keyboard
+    )
+
+@dp.message_handler(lambda message: message.text.lower() == 'рассчитать')
+async def main_menu(message: types.Message):
+    await message.answer("Выберите опцию:", reply_markup=inline_kb)
+
+@dp.callback_query_handler(lambda call: call.data == 'formulas')
+async def get_formulas(call: types.CallbackQuery):
+    formula_message = ("Формула Миффлина-Сан Жеора:\n"
+                           "Для женщин: 10 * вес (кг) + 6.25 * рост (см) - 5 * возраст (лет) - 161")
+    await call.message.answer(formula_message)
+
+@dp.callback_query_handler(lambda call: call.data == 'calories')
+async def set_age(call: types.CallbackQuery):
+    await call.message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=int(message.text))
+    await message.answer("Введите свой рост (в см):")
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=int(message.text))
+    await message.answer("Введите свой вес (в кг):")
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+    # Формула для женщин
+    calories = 10 * weight + 6.25 * growth - 5 * age - 161
+    await message.answer(f"Ваша норма калорий: {calories:.2f} ккал в сутки.")
+    await state.finish()
+
+@dp.message_handler(lambda message: message.text.lower() == 'купить')
+async def get_buying_list(message: types.Message):
+    products = []
+    for i in range(1, 5):
+        products.append({"name": f"Продукт {i}",
+                         "description": f"Описание {i}",
+                         "price": i * 100,
+                         "image_gp": f"./image{i}.png"}, )
+
+    for product in products:
+        long_string = f"Название: {product['name']} | Описание: {product['description']} | Цена: {product['price']}"
+        await message.answer_photo(photo=InputFile(product["image_gp"]), caption=long_string)
+
+    inline_pb = InlineKeyboardMarkup()
+    inline_pb.row(*(InlineKeyboardButton(product["name"], callback_data='product_buying') for product in products))
+    await message.answer("Выберите продукт для покупки:", reply_markup=inline_pb)
+
+@dp.callback_query_handler(lambda call: call.data == 'product_buying')
+async def send_confirm_message(call: types.CallbackQuery):
+    await call.message.answer("Вы успешно приобрели продукт!")
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
+
